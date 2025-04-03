e61a4fa00bdd381de7ed89de3de21cb45b57eb3d
Jhonatan Nobrega <jnobregadev@gmail.com>
2025-04-03 10:51:25 -0300

    Implemented routes and configured plugins to organize imports

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,9 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { globalIgnores } from 'eslint/config'
+// @ts-expect-error - eslint-plugin-import não tem tipos oficiais
+import importPlugin from 'eslint-plugin-import'
+import pluginVue from 'eslint-plugin-vue'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -18,9 +20,47 @@ export default defineConfigWithVueTs(
   pluginVue.configs['flat/essential'],
   {
     rules: {
-      'vue/multi-word-component-names': 0
-    }
+      'vue/multi-word-component-names': 0,
+    },
   },
   vueTsConfigs.recommended,
   skipFormatting,
+
+  // Plugin para ordenar imports
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js built-ins (fs, path, etc.)
+            'external', // Dependências externas (vue, react, etc.)
+            'internal', // Aliases (@/, ~/, etc.)
+            'parent', // Parent imports (`../`)
+            'sibling', // Sibling imports (`./`)
+            'index', // Index imports (`./index`)
+          ],
+          pathGroups: [
+            {
+              pattern: '{vue,vue-router,pinia}', // Agrupa Vue e seus pacotes
+              group: 'external',
+              position: 'before', // Coloca antes de outros imports externos
+            },
+            {
+              pattern: '@/**', // Aliases (ex: `@/components`)
+              group: 'internal',
+            },
+          ],
+          'newlines-between': 'always', // Linha em branco entre grupos
+          alphabetize: {
+            order: 'asc', // Ordem A-Z
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 )
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Page HomeView</h1>
+    <RouterLink to="/projects">Go to project</RouterLink>
+  </div>
+</template>
